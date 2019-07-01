08de641d3e8d42a9a8ced6faa6c7331fae550784
timkreuz <tim.kreuz@gmail.com>
2019-06-30 22:04:27 -0500

    add new files

---
+++ ufo-invaders/controllers/draw.js
@@ -0,0 +1,28 @@
+export function drawGame(game) {
+
+    displayScore(game);
+    drawObjects(game);
+}
+
+function displayScore(game) {
+    console.log(game);
+    game.infoCtx.clearRect(0, 0, game.infoCenter.width, game.infoCenter.height);
+    game.infoCtx.fillStyle = "#ffffff";
+    game.infoCtx.fillText("Score: " + game.stats.score + "          Abductions: " + game.stats.abductions +
+        "          Aliens Landed: " + game.stats.visitors, 8, 20);
+}
+
+function drawObjects(game) {
+    game.ctx.clearRect(0, 0, game.canvas.width, game.canvas.height);
+    drawObject(game.ctx, game.earth);
+    drawObject(game.ctx, game.sky);
+    drawObject(game.ctx, game.ufo);
+}
+
+function drawObject(ctx, object) {
+    ctx.beginPath();
+    ctx.rect(object.x, object.y, object.width, object.height);
+    ctx.fillStyle = object.color;
+    ctx.fill();
+    ctx.closePath();
+}
---
+++ ufo-invaders/controllers/game.js
@@ -0,0 +1,22 @@
+import {getEarth} from '../objects/earth.js';
+import {getSky} from '../objects/sky.js'; 
+import {getUFO} from '../objects/ufo.js'; 
+import {getStats} from '../objects/stats.js'; 
+
+export function getGame() {
+    var stats = getStats();
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    var infoCenter = document.getElementById("infoCenter");
+    var infoCtx = infoCenter.getContext("2d");
+    var earth = getEarth(canvas);
+    var sky = getSky(canvas);
+    var ufo = getUFO(canvas);
+
+    var game = {gameOver: false,
+                infoCenter: infoCenter, infoCtx: infoCtx,
+                canvas: canvas, ctx: ctx,
+                stats: stats,
+                earth: earth, sky: sky, ufo: ufo};
+    return game;
+}
