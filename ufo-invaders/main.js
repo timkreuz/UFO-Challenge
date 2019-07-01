27be470d74e06afe5a34c71c6ea3294952da8f4d
timkreuz <tim.kreuz@gmail.com>
2019-06-30 20:26:52 -0500

    A randomly flying ufo.

---
+++ ufo-invaders/main.js
@@ -0,0 +1,61 @@
+import {getEarth} from './objects/earth.js';
+import {getSky} from './objects/sky.js'; 
+import {getUFO, updateUFO} from './objects/ufo.js'; 
+import {stats} from './objects/stats.js'; 
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var infoCenter = document.getElementById("infoCenter");
+var info = infoCenter.getContext("2d");
+var earth = getEarth(canvas);
+var sky = getSky(canvas);
+var ufo = getUFO(canvas);
+
+function displayScore(stats) {
+    info.font = "16px Arial";
+    info.fillStyle = "#ffffff";
+    info.fillText("Score: " + stats.score + "          Abductions: " + stats.abductions +
+        "          Aliens Landed: " + stats.visitors, 8, 20);
+}
+
+function drawEarth(earth) {
+    ctx.beginPath();
+    ctx.rect(earth.x, earth.y, earth.width, earth.height);
+    ctx.fillStyle = earth.color;
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawSky(sky) {
+    ctx.beginPath();
+    ctx.rect(sky.x, sky.y, sky.width, sky.height);
+    ctx.fillStyle = sky.color;
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawUFO(ufo) {
+    ctx.beginPath();
+    ctx.rect(ufo.x, ufo.y, ufo.width, ufo.height);
+    ctx.fillStyle = ufo.color;
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+    if (stats.endOfGame) {
+        clearInterval(refreshInterval);
+    } else {
+        updateUFO(ufo);
+
+        info.clearRect(0, 0, canvas.width, canvas.height);
+        displayScore(stats);
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawEarth(earth);
+        drawSky(sky);
+        drawUFO(ufo);
+    }
+}
+
+var refreshInterval = setInterval(draw, 10);
---
+++ ufo-invaders/objects/ufo.js
@@ -0,0 +1,33 @@
+export function getUFO(canvas) {
+    return {
+        type: "rectangle",
+        x: 40, y: 40, width: 40, height: 5,
+        speedX: 2, speedY: 2,
+        maxLeft: 5, maxRight: canvas.width - 5,
+        maxUp: 5, maxDown: canvas.height /2,
+        color: '#ffff33',
+        timer: Date.now() + 3000
+    };
+}
+
+function randomize(ufo) {
+    ufo.speedX = (Math.floor(Math.random()* 90) - 45)/10;
+    ufo.speedY = (Math.floor(Math.random()* 90) - 45)/10;
+    ufo.timer = Date.now() + Math.floor(Math.random() * 1000) + 2000;
+}
+
+export function updateUFO(ufo) {
+    console.log(Date.now());
+    if (Date.now() > ufo.timer) randomize(ufo);
+
+    ufo.x = ufo.x + ufo.speedX;
+    ufo.y = ufo.y + ufo.speedY;
+    if (ufo.x + ufo.width > ufo.maxRight)
+        ufo.speedX = ufo.speedX * -1;
+    if (ufo.x < ufo.maxLeft)
+        ufo.speedX = ufo.speedX * -1;
+    if (ufo.y < ufo.maxUp)
+        ufo.speedY = ufo.speedY * -1;
+    if (ufo.y + ufo.height > ufo.maxDown)
+        ufo.speedY = ufo.speedY * -1;
+}
